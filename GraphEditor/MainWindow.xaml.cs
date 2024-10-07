fa93a17619d7545cce932832bcb1540a35e4c437
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-10-07 16:40:28 +0300

    Fixing button animations

--- GraphEditor/MainWindow.xaml.cs
+++ GraphEditor/MainWindow.xaml.cs
@@ -10,10 +10,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using static MaterialDesignThemes.Wpf.Theme;
 
 namespace GraphEditor
 {
@@ -81,6 +83,36 @@ namespace GraphEditor
             Console.WriteLine($"MousePosition: {currentMousePosition.X}, {currentMousePosition.Y}; Canvas.TopProperty: {ellipse.GetValue(Canvas.TopProperty)}");
             ellipse.SetValue(Canvas.TopProperty, currentMousePosition.Y - PointDimensions / 2);
             ellipse.SetValue(Canvas.LeftProperty, currentMousePosition.X - PointDimensions / 2 - 50);
+        }
+
+        private void ButtonAnimation(object sender, MouseEventArgs e)
+        {
+            //Button button = sender as Button;
+            //ColorAnimation buttonAnimation = new ColorAnimation();
+            //buttonAnimation.From = Colors.MediumPurple;
+            //buttonAnimation.To = Colors.Purple;
+            //buttonAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            //button.BeginAnimation(new PropertyPath(Button.BorderBrushProperty), buttonAnimation);
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            VisualStateManager.GoToState(sender as System.Windows.Controls.Button, "MouseOver", true);
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisualStateManager.GoToState(sender as System.Windows.Controls.Button, "Normal", true);
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            VisualStateManager.GoToState(sender as System.Windows.Controls.Button, "Pressed", true);
+        }
+
+        private void Button_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            VisualStateManager.GoToState(sender as System.Windows.Controls.Button, "Normal", true);
         }
     }
 }
