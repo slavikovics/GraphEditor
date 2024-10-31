50dbeeee7b9c5026a09f9be3254501aeb138135b
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-10-31 23:26:41 +0300

    RenamingWindow: adding event to share information

---
+++ GraphEditor/RenamingEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GraphEditor
+{
+    public class RenamingEventArgs : EventArgs
+    {
+        public bool _wasRenamed {  get; set; }
+        public string _newName { get; set; }
+
+        public RenamingEventArgs(bool wasRenamed, string newName)
+        {
+            _wasRenamed = wasRenamed;
+            _newName = newName;
+        }
+    }
+}
--- GraphEditor/RenamingWindow.xaml.cs
+++ GraphEditor/RenamingWindow.xaml.cs
@@ -1,36 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace GraphEditor
 {
-    /// <summary>
-    /// Interaction logic for InformationWindow.xaml
-    /// </summary>
     public partial class RenamingWindow : Window
     {
+        public event EventHandler<RenamingEventArgs> RenamingResult;
+
         private DispatcherTimer timer;
 
         public RenamingWindow()
         {
             InitializeComponent();
-        }
-
-        private void MaximizeWindowButton_Click(object sender, RoutedEventArgs e)
-        {
-            //if (this.WindowState == WindowState.Normal) this.WindowState = WindowState.Maximized;
-            //else this.WindowState = WindowState.Normal;
         }
 
         private void CollapseWindowButton_Click(object sender, RoutedEventArgs e)
@@ -74,46 +58,15 @@ namespace GraphEditor
         bool renamingButtonIsSelected = false;
         bool wasTextDeleted = false;
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            HiddenTextBox.Focus();
-            //if (!renamingButtonIsSelected)
-            //{
-            //    RenamedName.Background = new SolidColorBrush(Colors.LightBlue);
-            //    renamingButtonIsSelected = true;
-            //}
-            //else
-            //{
-            //    RenamedName.Background = null;
-            //    renamingButtonIsSelected = false;
-            //}
-        }
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (renamingButtonIsSelected && !wasTextDeleted)
-            {
-                RenamedName.Text = "";
-                
-                RenamedName.Text += e.ToString();
-                wasTextDeleted = true;
-            }
-        }
-
         private void UpdateRenamedName()
         {
             RenamedName.Text = HiddenTextBox.Text + "|";
         }
 
-        private void HiddenTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UpdateRenamedName();
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             HiddenTextBox.Focus();
-            HiddenTextBox.Text = RenamedName.Text.Substring(0, RenamedName.Text.Length - 1);
+            HiddenTextBox.Text = GetTextInput();
             HiddenTextBox.CaretIndex = HiddenTextBox.Text.Length;
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(800);
@@ -121,18 +74,45 @@ namespace GraphEditor
             timer.Start();
         }
 
+        private string GetTextInput()
+        {
+            return RenamedName.Text.Substring(0, RenamedName.Text.Length - 1);
+        }
+
         private void TimerTick(object sender, EventArgs e)
         {
             string newText = "";
             if (RenamedName.Text[RenamedName.Text.Length - 1] == '|')
             {
-                newText = RenamedName.Text.Substring(0, RenamedName.Text.Length - 1) + " ";
+                newText = GetTextInput() + " ";
             }
             else
             {
-                newText = RenamedName.Text.Substring(0, RenamedName.Text.Length - 1) + "|";
+                newText = GetTextInput() + "|";
             }
             RenamedName.Text = newText;
+        }
+
+        private void ApplyButtonClick(object sender, RoutedEventArgs e)
+        {
+            RenamingEventArgs renamingEventArgs = new RenamingEventArgs(true, GetTextInput());
+            RenamingResult?.Invoke(this, renamingEventArgs);
+        }
+
+        private void HiddenTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateRenamedName();
+        }
+
+        private void TextInputClick(object sender, RoutedEventArgs e)
+        {
+            HiddenTextBox.Focus();
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            RenamingEventArgs renamingEventArgs = new RenamingEventArgs(false, "");
+            RenamingResult?.Invoke(this, renamingEventArgs);
         }
     }
 }
