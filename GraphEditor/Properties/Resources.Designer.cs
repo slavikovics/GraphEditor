481dbce134680aba3e5db36cc0dc66e7fc3e95d9
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-11-13 03:01:14 +0300

    "Lonely" nodes saving and loading, TODO: add Task.Delay between nodes creations

--- GraphEditor/Properties/Resources.Designer.cs
+++ GraphEditor/Properties/Resources.Designer.cs
@@ -60,11 +60,307 @@ namespace GraphEditor.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to &lt;/div&gt;
+        ///.
+        /// </summary>
+        internal static string DivRelationEnd {
+            get {
+                return ResourceManager.GetString("DivRelationEnd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;div class=&quot;relation&quot;&gt;.
+        /// </summary>
+        internal static string DivRelationStart {
+            get {
+                return ResourceManager.GetString("DivRelationStart", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &quot;&gt;.
+        /// </summary>
+        internal static string EndOfTag {
+            get {
+                return ResourceManager.GetString("EndOfTag", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to firstNode.
+        /// </summary>
+        internal static string firstNode {
+            get {
+                return ResourceManager.GetString("firstNode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html lang=&quot;en&quot;&gt;
+        ///    &lt;style&gt;
+        ///body, html {
+        ///    margin: 0;
+        ///    padding: 0;
+        ///    width: 100%;
+        ///    height: 100%;
+        ///    font-family: &apos;Montserrat&apos;, sans-serif;
+        ///    background-color: rgb(13, 1, 19); /* Medium purple background */
+        ///}
+        ///
+        ///.container {
+        ///    display: flex;
+        ///    flex-direction: column;
+        ///    align-items: center;
+        ///    justify-content: flex-start;
+        ///    margin: 2%;
+        ///}
+        ///
+        ///.relation {
+        ///    display: flex;
+        ///    flex-direction: row;
+        ///    align-items: left;
+        ///    justify-content: flex-start;        /// [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string HtmlBeginWithCssStyles {
+            get {
+                return ResourceManager.GetString("HtmlBeginWithCssStyles", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to   &lt;footer&gt;
+        ///        &lt;span class=&quot;arrow&quot;&gt;Generated in GraphEditor&lt;/span&gt;
+        ///        &lt;a class=&quot;button&quot; href=&quot;https://github.com/slavikovics/GraphEditor&quot;&gt;GitHub&lt;/a&gt; 
+        ///    &lt;/footer&gt;
+        ///&lt;/body&gt;
+        ///&lt;/html&gt;
+        ///.
+        /// </summary>
+        internal static string HtmlEnd {
+            get {
+                return ResourceManager.GetString("HtmlEnd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;/h1&gt;
+        ///    &lt;/div&gt;
+        ///  .
+        /// </summary>
+        internal static string HtmlImageDivEnd {
+            get {
+                return ResourceManager.GetString("HtmlImageDivEnd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;/title&gt;
+        ///    &lt;link href=&quot;https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&amp;display=swap&quot; rel=&quot;stylesheet&quot;&gt;
+        ///    &lt;link rel=&quot;stylesheet&quot; href=&quot;styles.css&quot;&gt;
+        ///&lt;/head&gt;
+        ///&lt;body&gt;
+        ///    &lt;div class=&quot;container&quot;&gt;
+        ///        &lt;img src=&quot;graph.png&quot; alt=&quot;Graph Image&quot; class=&quot;main-image&quot;&gt;
+        ///        &lt;h1 class=&quot;header&quot;&gt;.
+        /// </summary>
+        internal static string HtmlMiddleWithImagePath {
+            get {
+                return ResourceManager.GetString("HtmlMiddleWithImagePath", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &quot; id=&quot;.
+        /// </summary>
+        internal static string NodeItemGroupIdTag {
+            get {
+                return ResourceManager.GetString("NodeItemGroupIdTag", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;div class=&quot;nodeItemGroup&quot;&gt;
+        ///            &lt;svg class=&quot;node&quot; id=&quot;_Слой_1&quot; data-name=&quot;Слой 1&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 56 56&quot;&gt;
+        ///                &lt;defs&gt;
+        ///                  &lt;style&gt;
+        ///                    .cls-1 {
+        ///                      fill: none;
+        ///                      stroke: #aea3d8;
+        ///                      stroke-miterlimit: 10;
+        ///                      stroke-width: 10px;
+        ///                    }
+        ///                  &lt;/style&gt;
+        ///                &lt;/defs&gt;
+        ///                &lt;circle class=&quot;cls-1&quot; cx=&quot;2 [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string NodeItemGroupStart {
+            get {
+                return ResourceManager.GetString("NodeItemGroupStart", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to nonOriented.
+        /// </summary>
+        internal static string nonOriented {
+            get {
+                return ResourceManager.GetString("nonOriented", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;svg class=&quot;edge&quot; id=&quot;_Слой_1&quot; data-name=&quot;Слой 1&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 482 85&quot;&gt;
+        ///            &lt;defs&gt;
+        ///              &lt;style&gt;
+        ///                .cls-1 {
+        ///                  fill: none;
+        ///                  stroke: #aea3d8;
+        ///                  stroke-miterlimit: 10;
+        ///                  stroke-width: 20px;
+        ///                }
+        ///              &lt;/style&gt;
+        ///            &lt;/defs&gt;
+        ///            &lt;rect class=&quot;cls-1&quot; x=&quot;10&quot; y=&quot;10&quot; width=&quot;462&quot; height=&quot;65&quot; rx=&quot;32.5&quot; ry=&quot;32.5&quot;/&gt;
+        ///          &lt;/svg&gt;.
+        /// </summary>
+        internal static string NonOrientedSvg {
+            get {
+                return ResourceManager.GetString("NonOrientedSvg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to HTML Files|*.html;*.htm|All Files|*.*.
         /// </summary>
         internal static string OpenFileDialogFilter {
             get {
                 return ResourceManager.GetString("OpenFileDialogFilter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to orientedPencil.
+        /// </summary>
+        internal static string orientedPencil {
+            get {
+                return ResourceManager.GetString("orientedPencil", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;svg class=&quot;edge&quot; id=&quot;_Слой_1&quot; data-name=&quot;Слой 1&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 497.97 40.61&quot;&gt;
+        ///            &lt;defs&gt;
+        ///              &lt;style&gt;
+        ///                .cls-1 {
+        ///                  fill: #aea3d8;
+        ///                  stroke-width: 0px;
+        ///                }
+        ///          
+        ///                .cls-2 {
+        ///                  fill: none;
+        ///                  stroke: #aea3d8;
+        ///                  stroke-miterlimit: 10;
+        ///                  stroke-width: 11px;
+        ///                }
+        ///              &lt;/style&gt;
+        ///          [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string OrientedPencilSvg {
+            get {
+                return ResourceManager.GetString("OrientedPencilSvg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to orientedSimple.
+        /// </summary>
+        internal static string orientedSimple {
+            get {
+                return ResourceManager.GetString("orientedSimple", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;svg class=&quot;edge&quot; id=&quot;_Слой_1&quot; data-name=&quot;Слой 1&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 500 44.25&quot;&gt;
+        ///            &lt;defs&gt;
+        ///              &lt;style&gt;
+        ///                .cls-1 {
+        ///                  fill: #9370db;
+        ///                  stroke-width: 0px;
+        ///                }
+        ///              &lt;/style&gt;
+        ///            &lt;/defs&gt;
+        ///            &lt;path class=&quot;cls-1&quot; d=&quot;m496.52,16.51l-31.81-15.82c-5.36-2.67-11.05,2.97-8.44,8.36l3.9,8.03H5c-2.76,0-5,2.24-5,5s2.24,5,5,5h455.23l-3.95,8.14c-2.62,5.39,3.08,11.03,8.44,8.36l31.81-15.82 [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string OrientedSimpleSvg {
+            get {
+                return ResourceManager.GetString("OrientedSimpleSvg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to PNG Image|*.png.
+        /// </summary>
+        internal static string SaveFileDialogPngFileFilter {
+            get {
+                return ResourceManager.GetString("SaveFileDialogPngFileFilter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save the graph folder.
+        /// </summary>
+        internal static string SaveFileDialogTitle {
+            get {
+                return ResourceManager.GetString("SaveFileDialogTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to secondNode.
+        /// </summary>
+        internal static string secondNode {
+            get {
+                return ResourceManager.GetString("secondNode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;/span&gt;
+        ///        &lt;/div&gt;.
+        /// </summary>
+        internal static string SpanAndDivEnd {
+            get {
+                return ResourceManager.GetString("SpanAndDivEnd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;/span&gt;.
+        /// </summary>
+        internal static string SpanRelationEnd {
+            get {
+                return ResourceManager.GetString("SpanRelationEnd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;span class=&quot;relationName&quot; class=&quot;.
+        /// </summary>
+        internal static string SpanRelationStart {
+            get {
+                return ResourceManager.GetString("SpanRelationStart", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &quot;&gt;.
+        /// </summary>
+        internal static string TagClosing {
+            get {
+                return ResourceManager.GetString("TagClosing", resourceCulture);
             }
         }
     }
